2a75669f92c1fa6f1443bb52c2ce8efe71505485
sabin79 <dsabin729@gmail.com>
2024-03-09 22:35:49 +0545

    hello

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        // tested with just a hot reload.
+
+        home: Scaffold(
+      body: Center(child: Text("Bloc Firebase")),
+    ));
+  }
+}
